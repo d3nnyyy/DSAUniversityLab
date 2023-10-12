8af3d4ef5d69caa59ed5cc1afc119261a095e931
d3nnyyy <d3nnn41k@gmail.com>
2023-10-12 11:37:19 +0300

    added docstrings to enhance code readability

--- is_tree_balanced/src/binary_tree.py
+++ is_tree_balanced/src/binary_tree.py
@@ -1,4 +1,18 @@
+"""
+Binary tree node implementation.
+"""
+
+
 class BinaryTree:
+    """
+    Initialize a new binary tree node.
+
+    Args:
+        value: The value to store in the node.
+        left: The left child node.
+        right: The right child node.
+    """
+
     def __init__(self, value, left=None, right=None):
         self.value = value
         self.left = left
