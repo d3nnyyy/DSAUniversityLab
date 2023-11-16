8e6e26e806cfa831e46e115ad7a727be09e1c65b
d3nnyyy <d3nnn41k@gmail.com>
2023-11-16 18:13:40 +0200

    modified tests by avoiding duplicate code

--- smallest_number_of_beer_types/test/test_smallest_number_of_beer_types.py
+++ smallest_number_of_beer_types/test/test_smallest_number_of_beer_types.py
@@ -2,8 +2,10 @@ import os
 import tempfile
 import unittest
 
-from smallest_number_of_beer_types.src.smallest_number_of_beer_types import convert_line_to_binary_array, \
-    transform_preferences, min_beers, read_beer_preferences
+from smallest_number_of_beer_types.src.smallest_number_of_beer_types import (
+    execute_beer_preferences,
+    NO_BEER_PREFERENCE_MSG, INVALID_NUM_OF_BEERS_MSG, INVALID_NUM_OF_EMPLOYEES_MSG, INVALID_PREFERENCES_LINE_MSG
+)
 
 
 class TestBeerPreferences(unittest.TestCase):
@@ -14,65 +16,52 @@ class TestBeerPreferences(unittest.TestCase):
         os.close(self.temp_fd)
         os.remove(self.temp_path)
 
-    def test_example_1(self):
-        # Test with the first example
+    def run_test_case(self, input_str, expected_result):
         with open(self.temp_path, 'w') as temp_file:
-            temp_file.write(
-                "2 2\nYN NY"
-            )
+            temp_file.write(input_str)
 
-        num_of_employees, num_of_beers, preferences_line = read_beer_preferences(self.temp_path)
+        result = execute_beer_preferences(self.temp_path)
 
-        array = convert_line_to_binary_array(preferences_line, num_of_beers)
+        self.assertEqual(result, expected_result)
 
-        adjacency_list_of_beer_preferences = transform_preferences(array)
+    def test_two_employees_which_like_different_beers(self):
+        self.run_test_case("2 2\nYN NY", 2)
 
-        result = min_beers(num_of_employees, adjacency_list_of_beer_preferences)
+    def test_where_theres_extra_beer(self):
+        self.run_test_case("6 3\nYNN YNY YNY NYY NYY NYN", 2)
 
-        self.assertEqual(result, 2)
+    def test_where_theres_extra_beer_2(self):
+        self.run_test_case("4 3\nYYN YYN YYN NNY", 2)
 
-    def test_example_2(self):
-        # Test with the second example
-        with open(self.temp_path, 'w') as temp_file:
-            temp_file.write("6 3\nYNN YNY YNY NYY NYY NYN")
+    def test_where_theres_four_employees_with_different_beer_preferences(self):
+        self.run_test_case("4 4\nYNNN NYNN NNYN NNNY", 4)
 
-        num_of_employees, num_of_beers, preferences_line = read_beer_preferences(self.temp_path)
+    def test_where_theres_beer_no_one_likes(self):
+        self.run_test_case("3 2\nYN YN YN", 1)
 
-        array = convert_line_to_binary_array(preferences_line, num_of_beers)
+    def test_where_theres_no_beer(self):
+        self.run_test_case("3 0\nYN YN YN", INVALID_NUM_OF_BEERS_MSG)
 
-        adjacency_list_of_beer_preferences = transform_preferences(array)
+    def test_where_theres_no_employees(self):
+        self.run_test_case("0 2\nYN YN YN", INVALID_NUM_OF_EMPLOYEES_MSG)
 
-        result = min_beers(num_of_employees, adjacency_list_of_beer_preferences)
+    def test_where_theres_too_many_employees(self):
+        self.run_test_case("50 2\nYN YN YN", INVALID_NUM_OF_EMPLOYEES_MSG)
 
-        self.assertEqual(result, 2)
+    def test_where_theres_too_many_beers(self):
+        self.run_test_case("3 50\nYN YN YN", INVALID_NUM_OF_BEERS_MSG)
 
-    def test_example_3(self):
-        # Test with the third example
-        with open(self.temp_path, 'w') as temp_file:
-            temp_file.write("4 3\nYYN YYN YYN NNY")
+    def test_where_theres_employee_which_doesnt_like_beer(self):
+        self.run_test_case("3 2\nYN NY NN", NO_BEER_PREFERENCE_MSG)
 
-        num_of_employees, num_of_beers, preferences_line = read_beer_preferences(self.temp_path)
+    def test_where_theres_invalid_preferences_line(self):
+        self.run_test_case("3 2\nYN NY N", INVALID_PREFERENCES_LINE_MSG)
 
-        array = convert_line_to_binary_array(preferences_line, num_of_beers)
+    def test_where_theres_invalid_preferences_line_2(self):
+        self.run_test_case("3 2\nYN NY NNN", INVALID_PREFERENCES_LINE_MSG)
 
-        adjacency_list_of_beer_preferences = transform_preferences(array)
-        result = min_beers(num_of_employees, adjacency_list_of_beer_preferences)
-
-        self.assertEqual(result, 2)
-
-    def test_example_4(self):
-        # Test with the fourth example
-        with open(self.temp_path, 'w') as temp_file:
-            temp_file.write("4 4\n YNNN NYNN NNYN NNNY")
-
-        num_of_employees, num_of_beers, preferences_line = read_beer_preferences(self.temp_path)
-
-        array = convert_line_to_binary_array(preferences_line, num_of_beers)
-
-        adjacency_list_of_beer_preferences = transform_preferences(array)
-        result = min_beers(num_of_employees, adjacency_list_of_beer_preferences)
-
-        self.assertEqual(result, 4)
+    def test_where_theres_invalid_preferences_line_3(self):
+        self.run_test_case("3 2", INVALID_PREFERENCES_LINE_MSG)
 
 
 if __name__ == '__main__':
