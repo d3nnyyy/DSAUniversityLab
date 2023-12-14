c6cfdcef1d933313af1eea192d1bcafece613016
d3nnyyy <d3nnn41k@gmail.com>
2023-12-15 00:05:50 +0200

    improved the readability

--- longest_wire/src/longest_wire.py
+++ longest_wire/src/longest_wire.py
@@ -1,29 +1,39 @@
+"""
+Longest Wire in the Village Problem
+"""
 import math
 
 
-def find_longest_wire(gap: int, poles: list[int]) -> float:
+def find_longest_wire(gap_width: int, pole_heights: list[int]) -> float:
+    """
+    Calculate the maximum length of wire needed to connect poles in the village.
+
+    :param gap_width: The width of the gap between the poles.
+    :param pole_heights: A list of integers representing the maximum possible height of each pole.
+    :return: The maximum length of wire needed to connect the poles.
+    """
+
+    # The longest paths to the top and bottom of the current pole.
     longest_path_to_top = 0
     longest_path_to_bottom = 0
 
     current_pole = 0
 
-    while current_pole < len(poles) - 1:
-        bottom_to_top = longest_path_to_bottom + math.sqrt(gap ** 2 + (poles[current_pole + 1] - 1) ** 2)
-        bottom_to_bottom = longest_path_to_bottom + gap
-        top_to_top = longest_path_to_top + math.sqrt(
-            gap ** 2 + abs(poles[current_pole] - poles[current_pole + 1]) ** 2)
-        top_to_bottom = longest_path_to_top + math.sqrt(gap ** 2 + (poles[current_pole] - 1) ** 2)
+    while current_pole < len(pole_heights) - 1:
+        # Calculate the longest paths from the bottom of the current pole to the top and bottom of the next pole.
+        bottom_to_top = longest_path_to_bottom + math.sqrt(gap_width ** 2 + (pole_heights[current_pole + 1] - 1) ** 2)
+        bottom_to_bottom = longest_path_to_bottom + gap_width
 
+        # Calculate the longest paths from the top of the current pole to the top and bottom of the next pole.
+        top_to_bottom = longest_path_to_top + math.sqrt(gap_width ** 2 + (pole_heights[current_pole] - 1) ** 2)
+        top_to_top = longest_path_to_top + math.sqrt(
+            gap_width ** 2 + abs(pole_heights[current_pole] - pole_heights[current_pole + 1]) ** 2)
+
+        # Update the longest paths to the top and bottom of the next pole.
         longest_path_to_top = max(bottom_to_top, top_to_top)
         longest_path_to_bottom = max(bottom_to_bottom, top_to_bottom)
 
         current_pole += 1
 
+    # Return the longest of the two paths.
     return max(longest_path_to_top, longest_path_to_bottom)
-
-
-print(find_longest_wire(2, [3, 3, 3]))
-print(find_longest_wire(4,
-                        [56, 18, 17, 94, 23, 7, 21, 94, 29, 54, 44, 26, 86, 79, 4, 15, 5, 91, 25, 17, 88, 66, 28, 2, 95,
-                         97, 60, 93, 40, 70, 75, 48, 38, 51, 34, 52, 87, 8, 62, 77, 35, 52, 3, 93, 34, 57, 51, 11, 39,
-                         72]))
